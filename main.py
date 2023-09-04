f8fc23d66317cd4eab4435887edd6b07d40bf0f9
Admin <miroslav.selivanov.1992@mail.ru>
2023-09-04 17:26:09 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# create mysql database mydb;
+# use mydb;
+# create table mycomputers(
+#     id int primary key auto_increment,
+#     name varchar(15) not null,
+#     year_production,
+#     varchar(10),
+#     store_count int);
+#
+# insert into mycomputers (name, year_prod, store_count) values(1,'Lenovo', 2010, 'China', 1),
+# (2,'Dell', 2012, 'USA', 2), (3,'Acer', 2013, 'China', 3),
+# (4,'HP', 2014, 'USA', 4), (5,'Asus', 2015, 'China', 5);
+#
+# select * from mycomputers
+# where name = 'Lenovo'
+# and year_prod = 2010;
+# select name is not null from mycomputers;
+# select name, year_prod from mycomputers
+# where year_prod null;
+
